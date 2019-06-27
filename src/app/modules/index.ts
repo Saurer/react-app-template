b41efaba3330a8c13b882136065d6c27038697fe
Frank Horrigan <redmmond@gmail.com>
2019-06-27 20:38:06 +1000

    Fixed observable action type

--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -1,11 +1,10 @@
-import { Action as ReduxAction } from 'redux';
 import { StateObservable } from 'redux-observable';
 import { Action, ActionCreator, Failure, Success } from 'typescript-fsa';
 import { RootState } from './reducer';
 import { Observable } from 'rxjs';
 
 export type Epic<TInput extends Action<any> = any, TOutput extends TInput = TInput> =
-    (action$: Observable<ReduxAction>, state$: StateObservable<RootState>, dependencies: {}) => Observable<TOutput>;
+    (action$: Observable<Action<any>>, state$: StateObservable<RootState>, dependencies: {}) => Observable<TOutput>;
 
 export type Reducer<TAction, TState> =
     TAction extends ActionCreator<Failure<infer P, infer E>> ? (state: TState, payload: Failure<P, E>) => TState :
