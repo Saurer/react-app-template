09ae7bd63a78811dbecf2f755abcc44068eed18b
Frank Horrigan <redmmond@gmail.com>
2019-08-27 21:45:22 +1000

    Migrate to ts-jest

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,14 @@
-const TEST_REGEX = '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$';
-
 module.exports = {
+    preset: 'ts-jest',
     setupFiles: ['<rootDir>/jest.setup.js'],
-    testRegex: TEST_REGEX,
-    transform: {
-        '^.+\\.tsx?$': 'babel-jest'
-    },
+    testEnvironment: 'node',
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
     collectCoverage: true,
-    moduleDirectories: ['node_modules', 'src/app']
+    moduleDirectories: ['node_modules', 'src/app'],
+    globals: {
+        'ts-jest': {
+            tsConfig: 'src/app/tsconfig.json'
+        }
+    }
 };
